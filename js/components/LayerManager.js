c79bb50fc6578f6161a0c8f91ca9e45b3bad22d4
liao <2735398076@qq.com>
2017-12-07 19:22:10 +0800

    解决箭头符号调用问题

--- js/components/LayerManager.js
+++ js/components/LayerManager.js
@@ -7,10 +7,29 @@ const FormItem = Form.Item;
 const  Option = Select.Option;
 
 class layerM extends React.Component{
-    componentDidMount() {
+    constructor(props){
+        super(props);
+        this.state = {
+            currency:''
+        }
+    }
+ /*   componentDidMount() {
         // To disabled submit button at the beginning.
         //this.props.form.validateFields();
-    }
+    }*/
+    handleCurrencyChange = (currency) => {
+        if (!('value' in this.props)) {
+            this.setState({ currency });
+        }
+        this.triggerChange({ currency });
+    };
+    triggerChange = (changedValue) => {
+        // Should provide an event to pass value to Form.
+        const onChange = this.props.onChange;
+        if (onChange) {
+            onChange(Object.assign({}, this.state, changedValue));
+        }
+    };
     handleSubmit (e) {
         e.preventDefault();
         this.props.form.validateFields((err, values) => {
@@ -18,12 +37,12 @@ class layerM extends React.Component{
                 console.log('Received values of form: ', values);
             }
         });
-    }
+    };
     render(){
         return (
         <Form layout="inline" onSubmit = { this.handleSubmit }>
             <Form.Item>
-                <Select style={{width:200}}>
+                <Select value={this.state.currency} onChange={this.handleCurrencyChange} style={{width:200}}>
                     <Option value="task">task1</Option>
                     <Option value="task1">task1</Option>
                     <Option value="task2">task3</Option>
